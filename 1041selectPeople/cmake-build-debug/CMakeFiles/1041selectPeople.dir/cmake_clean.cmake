327969587830b664b8a37eb66c69672fd4d44a07
wpcwzy <wpc.369623245@live.cn>
2019-01-24 21:39:22 +0800

    Windows edited

---
+++ 1041selectPeople/cmake-build-debug/CMakeFiles/1041selectPeople.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1041selectPeople.dir/main.cpp.obj"
+  "1041selectPeople.pdb"
+  "1041selectPeople.exe"
+  "1041selectPeople.exe.manifest"
+  "1041selectPeople.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1041selectPeople.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1041selectPeople/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int m,n;
+    scanf_s("%d%d",&m,&n);
+    printf("%d%d",m,n);
+    return 0;
+}
